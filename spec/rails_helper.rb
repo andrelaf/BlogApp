1c8bc86311f2851356eee130c7c7c2cbd1462dbc
André Lara Amorim <andre.amorim.si@hotmail.com>
2017-02-15 21:06:45 +0000

    Restrict access with controller actions

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,4 +56,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  
+  #Tive que adicionar essa linha para funcionar teste controller devise. macros.rb
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  
 end
