d0afe8d0dfa3d1610710c1ff982c4948129aaf5b
André Lara Amorim <andre.amorim.si@hotmail.com>
2017-02-21 21:53:49 +0000

    Force signin or signup before posting comments

--- spec/features/adding_comments_spec.rb
+++ spec/features/adding_comments_spec.rb
@@ -15,7 +15,7 @@ RSpec.feature "Adding Reviews to Articles" do
     fill_in "New Comment", with: "An awesome article"
     click_button "Add Comment"
     
-    expect(page).to have_content("Comment has ben created")
+    expect(page).to have_content("Comment has been created")
     expect(page).to have_content("An awesome article")
     expect(current_path).to eq(article_path(@article.comments.last.id))
     
